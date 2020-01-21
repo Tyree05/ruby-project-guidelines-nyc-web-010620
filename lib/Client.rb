81744920760fe3f517d5da25651d037e1325533a
Anil Gurung <anil_grg@hotmail.com>
2020-01-21 14:55:26 -0500

    Update Client.rb

--- lib/Client.rb
+++ lib/Client.rb
@@ -1,2 +1,10 @@
-class Client 
-end
+class Client < ActiveRecord::Base
+    def change
+        create_table :clients do |t|
+          t.name :name
+          t. :description
+     
+          t.timestamps
+        end
+      end
+    end
