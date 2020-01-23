b060b958c6ec25c5ef8beabf071dcffde0049dd3
Anil Gurung <anil_grg@hotmail.com>
2020-01-23 14:39:07 -0500

    ee

--- lib/SignUp.rb
+++ lib/SignUp.rb
@@ -1,26 +1,20 @@
 
 def ProgrammerChoice
     prompt = TTY::Prompt.new
-<<<<<<< HEAD
-    prompt.ask('Create a username:')
-    d => gets.chomp
-    Programmer.create(name: d)
-=======
     if (prompt.ask('Create a username:') == "exit!")
         return prompt_user
     else
     Programmer.create(name: prompt.ask('Create a username:'))
-    end
     system("clear")
     puts "Creation successful"
     puts "(press enter to return to main screen)"
     gets.chomp
     prompt_user
->>>>>>> 2a3ff3dd320f6d5089c064092df29ac4dac3ee63
+    end
 end
 
 def SignUp_choice
-    prompt = TTY::Prompt.new
+     prompt = TTY::Prompt.new
     prompt.select('Who are you?') do |menu|
         menu.choice 'Programmer', -> { ProgrammerChoice() }
         menu.choice 'Client', -> { ClientChoice() }
@@ -30,14 +24,17 @@ def SignUp_choice
 
   def ClientChoice
     prompt = TTY::Prompt.new
-   if (prompt.ask('Create a username:') == "exit!")
-    return prompt_user
+    y = prompt.ask('Create a username:')
+    if (y == "exit!")
+       return prompt_user
    else
-    Client.create(name: prompt.ask('Create a username:'))
-   end
+    
+    Client.create(name: y)
     system("clear")
     puts "Creation successful"
     puts "(press enter to return to main screen)"
     gets.chomp
     prompt_user
+   end
 end
+
--- lib/models/Login.rb
+++ lib/models/Login.rb
@@ -1,12 +1,90 @@
 def LoginChoice
+    # prompt = TTY::Prompt.new
+    l = prompt.ask('Please enter Username')
+    if l == "!!!" 
+       prompt_user
+    elsif l == "!!"
+       system("clear")
+    end
+
+#     if Programmer.find_by(name: l)
+#         system("clear")
+#         Login_Programmer()
+       
+
+#     elsif Client.find_by(name: l)
+#        system("clear")
+#        Login_Client()
+
+#     else
+#         system("clear")
+#         puts "name not found"
+#         LoginChoice()
+#     end
+end
+
+
+def Test
+    prompt = TTY::Prompt.new
+    prompt.select('Who are you?') do |menu|
+        menu.choice 'Programmer', -> { ProTest() }
+        menu.choice 'Client', -> { CleintTest() }
+    
+    end
+  end
+
+def ProTest
     prompt = TTY::Prompt.new
     l = prompt.ask('Please enter Username')
     if Programmer.find_by(name: l)
         system("clear")
-        puts "name found!"
+        Login_Programmer()
     else
-        system("clear")
-        puts "name not found"
-        LoginChoice()
+        puts "Name not found"
+        ProTest()
     end
+end
+
+def CleintTest
+    prompt = TTY::Prompt.new
+    l = prompt.ask('Please enter Username')
+    if Client.find_by(name: l)
+       system("clear")
+       Login_Client()
+    else
+        puts "Name not found"
+        CleintTest()
+    end
+end
+
+
+
+def Login_Client
+    prompt = TTY::Prompt.new
+    prompt.select('What would you like to do today?') do |menu|
+        menu.choice 'Delete project', -> {Login_Client() }
+        menu.choice 'Create project', -> {Login_Client()}
+        menu.choice 'View projects', -> {Login_Client()}
+        menu.choice 'Exit', -> {system("clear")}
+      
+    end
+
+  
+end
+
+
+def Login_Programmer
+    prompt = TTY::Prompt.new
+    prompt.select('What would you like to do today?') do |menu|
+        menu.choice 'Delete project', -> {Login_Programmer() }
+        menu.choice 'View projects', -> {Login_Programmer()}
+        menu.choice 'Exit', -> {system("clear")}
+      
+    end
+
+  
+end
+
+def ViewProjects
+    
 end
